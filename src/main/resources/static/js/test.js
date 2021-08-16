c5ebc0c44b27affd06a4b3c421a5d0f02f0bb58f
vikki111 <132354a@mail.ru>
2021-08-16 22:11:58 +0400

    Улучшение верстки

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -6,6 +6,8 @@ let edgeLabels = {};
 
 let greenNodeIds = {};
 
+let greenEdgeIds = {};
+
 let redNodeIds = {};
 
 let selectedEdgeId;
@@ -14,226 +16,276 @@ var nId = 0;
 
 function processData(test) {
 
-  if(graph.isForceAtlas2Running())
-    graph.killForceAtlas2();
+    if (graph.isForceAtlas2Running())
+        graph.killForceAtlas2();
 
-  if(atlasTimeout)
-    clearTimeout(atlasTimeout);
+    if (atlasTimeout)
+        clearTimeout(atlasTimeout);
 
-  const data = {};
+    const data = {};
 
-  $('#input').val().split('\n').forEach(function(line){
-    if(line.includes(':'))
-      data[line.split(':')[0].trim()] = line.split(':')[1].split(',').map(x => x.trim()).filter(x => x);
-  });
-
-  const dataKeys = Object.keys(data);
-  const tagIds = [];
-
-  const graphData = {
-    nodes:[],
-    edges:[]
-  };
-
-  dataKeys.forEach(function(key){       //добавление вершин слева
-    if(!tagIds.includes(key)){            //если их не было еще
-        graphData.nodes.push({
-          id: key,
-          label: key,
-          size: 10,
-          color: '#00838F',
-          x:Math.random(),
-          y:Math.random(),
-        });
-        tagIds.push(key);
-    }
-    data[key].forEach(function(tag){       //добавление вершин справа
-      if(!tagIds.includes(tag)){             //если их не было еще
-        graphData.nodes.push({
-          id: tag,
-          label: tag,
-          size: 10,
-          color: '#00695C',
-          x:Math.random(),
-          y:Math.random()
-        });
-        tagIds.push(tag);
-      }
-      if(key === tag) {
-          graphData.edges.push({ //добавление связей
-          id: key+tag,
-          source: key,
-          size: 12,
-          target: tag,
-          color: '#ccc',
-          hover_color: '#000',
-          type: 'curve'
-        });
-      } else {
-          graphData.edges.push({ //добавление связей
-          id: key+tag,
-          source: key,
-          size: 12,
-          target: tag,
-          color: '#ccc',
-          hover_color: '#000'
-        });
-      }
+    $('#input').val().split('\n').forEach(function(line) {
+        if (line.includes(':'))
+            data[line.split(':')[0].trim()] = line.split(':')[1].split(',').map(x => x.trim()).filter(x => x);
     });
-  });
 
-if(edgeLabels != null ) {
-    console.log('!!!!!');
-    graphData.edges.forEach(function(edge){
-        if(edgeLabels[edge.id] != null) {
-            edge.label = edgeLabels[edge.id];
+    const dataKeys = Object.keys(data);
+    const tagIds = [];
+
+    const graphData = {
+        nodes: [],
+        edges: []
+    };
+
+    dataKeys.forEach(function(key) { //добавление вершин слева
+        if (!tagIds.includes(key)) { //если их не было еще
+            graphData.nodes.push({
+                id: key,
+                label: key,
+                size: 10,
+                color: '#00838F',
+                x: Math.random(),
+                y: Math.random(),
+            });
+            tagIds.push(key);
         }
+        data[key].forEach(function(tag) { //добавление вершин справа
+            if (!tagIds.includes(tag)) { //если их не было еще
+                graphData.nodes.push({
+                    id: tag,
+                    label: tag,
+                    size: 10,
+                    color: '#00695C',
+                    x: Math.random(),
+                    y: Math.random()
+                });
+                tagIds.push(tag);
+            }
+            if (key === tag) {
+                graphData.edges.push({ //добавление связей
+                    id: key + tag,
+                    source: key,
+                    size: 12,
+                    target: tag,
+                    color: '#ccc',
+                    hover_color: '#000',
+                    type: 'curve'
+                });
+            } else {
+                graphData.edges.push({ //добавление связей
+                    id: key + tag,
+                    source: key,
+                    size: 12,
+                    target: tag,
+                    color: '#ccc',
+                    hover_color: '#000'
+                });
+            }
+        });
     });
+
+    if (edgeLabels != null) {
+        console.log('!!!!!');
+        graphData.edges.forEach(function(edge) {
+            if (edgeLabels[edge.id] != null) {
+                edge.label = edgeLabels[edge.id];
+            }
+        });
+    }
+
+
+    //  graph.graph.clear().read(graphData);
+
+    //  graph.settings({
+    //    edgeColor: 'default',
+    //    defaultEdgeColor: '#9E9E9E',
+    //    labelThreshold:0
+    //  });
+
+    //  graph.camera.goTo({
+    //    x:0,
+    //    y:0,
+    //    ratio:1.1,
+    //    angle:0
+    //  });
+
+    if (dataKeys.length) {
+        graph.startForceAtlas2({
+            iterationsPerRender: 1000,
+            outboundAttractionDistribution: true
+        });
+
+        atlasTimeout = setTimeout(function() {
+            graph.killForceAtlas2();
+        }, 5000);
+    } else {
+        graph.refresh();
+    }
 }
 
 
-//  graph.graph.clear().read(graphData);
 
-//  graph.settings({
-//    edgeColor: 'default',
-//    defaultEdgeColor: '#9E9E9E',
-//    labelThreshold:0
-//  });
-
-//  graph.camera.goTo({
-//    x:0,
-//    y:0,
-//    ratio:1.1,
-//    angle:0
-//  });
-
-  if(dataKeys.length){
-    graph.startForceAtlas2({
-      iterationsPerRender:1000,
-      outboundAttractionDistribution:true
-    });
-
-    atlasTimeout = setTimeout(function(){
-      graph.killForceAtlas2();
-    }, 5000);
-  }else{
-    graph.refresh();
-  }
-}
-
-
-
-$(document).ready(function(){
+$(document).ready(function() {
 
     graph = new sigma({
-      renderer: {
-        container: document.getElementById('graph-container'),
-        type: 'canvas'
-      },
-      settings: {
-        doubleClickEnabled: false,
-        minEdgeSize: 0.5,
-        maxEdgeSize: 4,
-        maxNodeSize: 16,
-        enableEdgeHovering: true,
-        edgeHoverColor: 'edge',
-        defaultEdgeHoverColor: '#000',
-        edgeHoverSizeRatio: 1,
-        edgeHoverExtremities: true,
-        labelThreshold:0,
-        edgeLabelSize: 'fixed',
-        defaultEdgeLabelSize: 18,
-        defaultLabelSize: 20
-      }
+        renderer: {
+            container: document.getElementById('graph-container'),
+            type: 'canvas'
+        },
+        settings: {
+            doubleClickEnabled: false,
+            minEdgeSize: 0.5,
+            maxEdgeSize: 4,
+            maxNodeSize: 16,
+            enableEdgeHovering: true,
+            edgeHoverColor: 'edge',
+            defaultEdgeHoverColor: '#000',
+            edgeHoverSizeRatio: 1,
+            edgeHoverExtremities: true,
+            labelThreshold: 0,
+            edgeLabelSize: 'fixed',
+            defaultEdgeLabelSize: 18,
+            defaultLabelSize: 20
+        }
     });
     var dragListener = sigma.plugins.dragNodes(graph, graph.renderers[0]);
 
     //  graph = new sigma('container');
-      processData();
+    processData();
 
-      let timeout;
-      $('#input').on('input', function(){
+    let timeout;
+    $('#input').on('input', function() {
         clearTimeout(timeout);
-        timeout = setTimeout(function(){
-          processData();
+        timeout = setTimeout(function() {
+            processData();
         }, 500);
-      });
+    });
 
-      graph.bind('doubleClickEdge', function(e) {
+    graph.bind('doubleClickEdge', function(e) {
         console.log(e.data.edge.id);
         selectedEdgeId = e.data.edge.id;
-      });
+    });
 
-      graph.bind('clickNode', function(e){
-          console.log(e.data.node.id);
-          if (e.data.node.isSelected) {
-              e.data.node.color = "#000";
-              e.data.node.isSelected = false;
-            } else {
-              e.data.node.color = "#0A0"; //зеленый
-              e.data.node.isSelected = true;
-              greenNodeIds[0] = e.data.node.id;
+    //      graph.bind('clickNode', function(e){ //выделение вершины ЛКМ и окрашивание в зеленый
+    //          console.log(e.data.node.id);
+    //          if (e.data.node.isSelected) {
+    //              e.data.node.color = "#000";
+    //              e.data.node.isSelected = false;
+    //            } else {
+    //              e.data.node.color = "#0A0"; //зеленый
+    //              e.data.node.isSelected = true;
+    //              greenNodeIds[0] = e.data.node.id;
+    //            }
+    //            graph.refresh();
+    //      });
+
+    graph.bind('rightClickEdge', function(e) {
+        console.log(e.data.edge.id);
+        if (e.data.edge.isSelected) {
+            e.data.edge.color = "#000";
+            e.data.edge.isSelected = false;
+        } else {
+            e.data.edge.color = "#0A0"; //зеленый
+            e.data.edge.isSelected = true;
+            greenEdgeIds[0] = e.data.edge.id;
+        }
+        graph.refresh();
+    });
+
+    graph.bind('rightClickNode', function(e) {
+        console.log(e.data.node.id);
+        if (e.data.node.isSelected) {
+            e.data.node.color = "#000";
+            e.data.node.isSelected = false;
+            if (redNodeIds[0] == e.data.node.id) {
+                redNodeIds[0] = null;
             }
-            graph.refresh();
-      });
-
-      graph.bind('rightClickNode', function(e){
-            console.log(e.data.node.id);
-            if (e.data.node.isSelected) {
-                e.data.node.color = "#000";
-                e.data.node.isSelected = false;
-                if(redNodeIds[0] == e.data.node.id) {
-                    redNodeIds[0] = null;
-                }
-              } else {
-                e.data.node.color = "#f00"; //красный
-                e.data.node.isSelected = true;
-                if(redNodeIds[0] == null) {
-                    redNodeIds[0] = e.data.node.id;
-                } else {
+        } else {
+            e.data.node.color = "#f00"; //красный
+            e.data.node.isSelected = true;
+            if (redNodeIds[0] == null) {
+                redNodeIds[0] = e.data.node.id;
+            } else {
+                if(existsEdge(redNodeIds[0] + "-" + e.data.node.id) == false) {
                     graph.graph.addEdge({ //добавление связей
-                      id: redNodeIds[0]+""+e.data.node.id,
-                      source: redNodeIds[0],
-                      size: 12,
-                      target: e.data.node.id,
-                      color: '#ccc',
-                      hover_color: '#000'
+                        id: redNodeIds[0] + "-" + e.data.node.id,
+                        source: redNodeIds[0],
+                        size: 12,
+                        target: e.data.node.id,
+                        color: '#ccc',
+                        hover_color: '#000',
+                        type: "arrow"
                     });
-                    graph.graph.nodes().forEach(function(node) {
-                        if(node.id == e.data.node.id || node.id == redNodeIds[0]) {
-                            node.color = '#000';
-                            node.isSelected = false;
-                        }
-                     });
-                    redNodeIds[0]= null;
                 }
-              }
-              graph.refresh();
+                graph.graph.nodes().forEach(function(node) {
+                    if (node.id == e.data.node.id || node.id == redNodeIds[0]) {
+                        node.color = '#000';
+                        node.isSelected = false;
+                    }
+                });
+                redNodeIds[0] = null;
+            }
+        }
+        graph.refresh();
+    });
+
+    function existsNode(id) {
+        graph.graph.nodes().forEach(function(node) {
+            if (node.id == id) {
+                return true;
+            }
         });
+        return false;
+    }
+
+    function existsEdge(id) {
+        var flag = false;
+        graph.graph.edges().forEach(function(edge) {
+            if (edge.id == id) {
+                flag = true;
+            }
+        });
+        return flag;
+    }
+
+    function existsInverseEdge(id) {
+            var flag = false;
+            var arrayOfStrings = id.split("-");
+            graph.graph.edges().forEach(function(edge) {
+                if (edge.id == arrayOfStrings[0]) {
+                    flag = true;
+                }
+            });
+            return flag;
+        }
+
 
     $('#subButton').click(function() {
-       console.log($('#inputEdge').val());
-       edgeLabels[selectedEdgeId] = $('#inputEdge').val();
-       console.log(Object.keys(edgeLabels));
-       console.log(Object.values(edgeLabels));
+        console.log($('#inputEdge').val());
+        edgeLabels[selectedEdgeId] = $('#inputEdge').val();
+        console.log(Object.keys(edgeLabels));
+        console.log(Object.values(edgeLabels));
     });
 
     $('#jsonButton').click(function() {
-//       console.log(JSON.stringify({nodes: graph.graph.nodes(), edges: graph.graph.edges()}));
+        //       console.log(JSON.stringify({nodes: graph.graph.nodes(), edges: graph.graph.edges()}));
         some();
     });
 
     async function some() {
-      let response = await fetch('/savejson', {
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json;charset=utf-8'
-       },
-       body: JSON.stringify({nodes: graph.graph.nodes(), edges: graph.graph.edges()})
-         });
+        let response = await fetch('/savejson', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                nodes: graph.graph.nodes(),
+                edges: graph.graph.edges()
+            })
+        });
 
-         let result = await response.json();
-//         alert(result.toString());
+        let result = await response.json();
+        //         alert(result.toString());
         console.log(result);
     }
 
@@ -241,42 +293,47 @@ $(document).ready(function(){
 
     dom.addEventListener('contextmenu', event => event.preventDefault());
 
-      dom.addEventListener('dblclick', function(e) {
+    dom.addEventListener('dblclick', function(e) {
 
-          var x,
-              y,
-              p,
-              id,
-              neighbors;
+        var x,
+            y,
+            p,
+            id,
+            neighbors;
 
-         x = sigma.utils.getX(e) - dom.offsetWidth / 2;
-         y = sigma.utils.getY(e) - dom.offsetHeight / 2;
+        x = sigma.utils.getX(e) - dom.offsetWidth / 2;
+        y = sigma.utils.getY(e) - dom.offsetHeight / 2;
 
-     p = graph.camera.cameraPosition(x, y);
+        p = graph.camera.cameraPosition(x, y);
         x = p.x;
         y = p.y;
         var test = ++nId;
 
-            graph.graph.addNode({
-              id: test,
-              size: 50,
-              label: test + "",
-              x: x + Math.random() / 10,
-              y: y + Math.random() / 10,
-              dX: 0,
-              dY: 0,
-            });
-            graph.refresh();
-        }, false);
-
-
-        $('html').keyup(function(e){
-            if(e.keyCode == 46) {
-                //greenNodeIds[0]
-                graph.graph.dropNode(greenNodeIds[0]);
-                graph.refresh();
-            }
+        graph.graph.addNode({
+            id: test,
+            size: 50,
+            label: test + "",
+            x: x + Math.random() / 10,
+            y: y + Math.random() / 10,
+            dX: 0,
+            dY: 0,
         });
+        graph.refresh();
+    }, false);
+
+
+    $('html').keyup(function(e) {
+        if (e.keyCode == 46) {
+            if (redNodeIds[0] != null) {
+                graph.graph.dropNode(redNodeIds[0]);
+                redNodeIds[0] = null;
+            }
+            if (greenEdgeIds[0] != null) {
+                graph.graph.dropEdge(greenEdgeIds[0]);
+                greenEdgeIds = null;
+            }
+            graph.refresh();
+        }
+    });
 
 });
-
